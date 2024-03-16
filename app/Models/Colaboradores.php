dc0851aa0b6e1262e0df021c3c4f06897999953a
vinicius00franco <vincus.franco.silva@gmail.com>
2024-03-16 12:38:19 -0300

    criando as tabelas e migrações

---
+++ app/Models/Colaboradores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Colaboradores extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_03_16_152231_create_unidades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome_fantasia');
+            $table->string('razao_social');
+            $table->string('cnpj')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades');
+    }
+}
